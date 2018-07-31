b560217b01a062c2526e93240c5bb46d72e39fd5
mannyrobin <69haiqian@gmail.com>
2018-07-31 15:47:11 +0300

    Added Frindly Leagues constant types for the actions \ reducers

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
+// ** Auth **
 export const EMAIL_CHANGED = 'email_changed';
 export const PASSWORD_CHANGED = 'password_changed';
 export const LOGGING_USER_IN = 'logging_user_in';
@@ -5,3 +6,8 @@ export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 export const FORGOT_PASSWORD = 'forgot_password';
 export const PASSWORD_RECOVERY = 'password_recovery';
+
+// ** Friendly Leagues **
+
+export const FRIENDLY_LEAGUE_NAME_CHANGED = 'friendly_league_name_changed';
+export const NEW_FRIENDLY_LEAGUE = 'new_friendly_league';

fceb8c4ac1fad2259470e2b0f40998fad437e032
mannyrobin <69haiqian@gmail.com>
2018-10-11 19:26:35 +0300

    Main league header now shows the league's level.

--- src/components/LeagueHeader.js
+++ src/components/LeagueHeader.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import firebase from 'react-native-firebase';
 
+import { locali } from '../../locales/i18n';
+
 export default ({ league }) => (
     <View style={{ flexDirection: 'row', justifyContent: 'center', flex: 1 }}>
         <View style={{ justifyContent: 'center', marginRight: 5 }}>
-            <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{league.friendlyLeagueName}</Text>
+            <Text style={{ fontWeight: 'bold', fontSize: 16 }}>
+                {league.level ? 
+                    locali('main_leagues.league_level', { leagueLevel: league.level }) : 
+                    league.friendlyLeagueName}
+            </Text>
         </View>
         <View
             style={{
@@ -18,9 +24,10 @@ export default ({ league }) => (
             }}
         >
             <Text style={{ fontWeight: 'bold', fontSize: 16, marginRight: 5 }}>{
-                league.participants
-                .find(participant => participant.uid === firebase.auth().currentUser.uid)
-                .coins
+                 league.participants
+                 .find(participant =>
+                     participant.uid === firebase.auth().currentUser.uid)
+                 .coins
             }</Text>
             <Image
                 source={require('../images/Currency2Small.png')}
